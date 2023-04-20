9c3245aa8b8b0e5a8e6aff97aa0804046b191143
Vasilij U <vasya-1001@yandex.ru>
2023-04-20 16:20:50 +0500

    Zadacha 23 completed

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Numbers(Read());
+
+int Read()
+{
+    System.Console.WriteLine("Enter number: ");
+    int N = int.Parse(System.Console.ReadLine());
+    return N;
+}
+
+void Numbers(int N)
+{
+    int n = 1;
+    while (n<=N)
+    {
+        double d = Math.Pow(n,3);
+        System.Console.Write($"{d} ");
+        n++;
+    }
+}
