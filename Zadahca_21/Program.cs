83c61a07938934829cd5aa65e366b6133d3bd498
Vasilij U <vasya-1001@yandex.ru>
2023-04-20 16:12:36 +0500

    Zadacha 21 completed

---
+++ Zadahca_21/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+
+int Read(string arg)
+{
+    int num;
+    System.Console.WriteLine($"input {arg}: ");
+    while ((!int.TryParse(System.Console.ReadLine(), out num)))
+    {
+        System.Console.WriteLine("Not a number");
+    }
+    return num;
+}
+
+double Length()
+{
+    int x1 = Read("x1");
+    int y1 = Read("y1");
+    int z1 = Read("z1");
+    int x2 = Read("x2");
+    int y2 = Read("y2");
+    int z2 = Read("z2");
+    double L = Math.Round((Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2))),3);
+    return L;
+}
+
+void Answ()
+{
+    double L = Length();
+    System.Console.WriteLine($"Vector length = {L}");
+}
+
+Answ();
