73762c92250dfe3ccea1ab1ba0cdb3811c0c8876
Vasilij U <vasya-1001@yandex.ru>
2023-04-20 15:58:11 +0500

    Zadacha 19 completed

---
+++ Zadacha_19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+System.Console.WriteLine("Enter number from 10000 to 99999: ");
+int a = int.Parse(System.Console.ReadLine());
+
+if ((a>=10000)&(a<=99999))
+{
+
+    if (((a/10000)%10)==((a/1)%10))
+    {
+        if (((a/1000)%10)==((a/10)%10)) System.Console.WriteLine("Yes");
+    }
+    else System.Console.WriteLine("No");
+    
+}
+else System.Console.WriteLine("No");
